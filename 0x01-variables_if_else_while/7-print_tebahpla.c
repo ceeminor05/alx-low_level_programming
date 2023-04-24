32d586221f0d4361b12f4423868f3253d307e87c
ceeminor05 <julianaijekeye@gmail.com>
2023-04-24 15:52:49 -0700

    like

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print alphabets backwards
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'z'; alph >= 'a'; alph--)
+	{
+		putchar(alph);
+	}
+	putchar('\n')
+
+		return (0);
+}
