7aadf4edb110e277b35bed943db339223619d3c4
ceeminor05 <julianaijekeye@gmail.com>
2023-04-28 06:08:18 -0700

    like

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	for (char c = 0; c < 10; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+	return (0);
+}
