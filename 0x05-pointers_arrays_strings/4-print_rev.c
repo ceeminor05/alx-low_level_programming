20a87158f4f3b663e739668f2310b4ac3de4ea5e
ceeminor05 <julianaijekeye@gmail.com>
2023-05-03 06:22:33 -0700

    like

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: inputed string
+ * Return: reversed string
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+			break;
+		count++;
+	}
+
+	for (count = count - 1; count >= 0; count--)
+		_putchar([count]);
+
+	_putchar('\n');
