42016db0b614969a31408db6933e44cf298defd8
ceeminor05 <julianaijekeye@gmail.com>
2023-05-04 05:10:01 -0700

    concatenates strings

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * _putchar - write trhe character c to stdout 
+ * @c: character to print
+ * Return: 1 if true and 0 is false
+ * on error -  return -1
+ */
+int _putchar(char c)
+{
+        return (write (1, &c, 1));
+}
